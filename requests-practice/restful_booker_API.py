46177976abc8d4fc8f729a94e7524ed47ba32260
Daniel-dev-god <danredrock177@gmail.com>
2023-07-25 22:04:27 +0200

    Please push

--- requests-practice/restful_booker_API.py
+++ requests-practice/restful_booker_API.py
@@ -1,5 +1,6 @@
 from requests import request, Response
 from typing import Dict, Any
+import requests
 
 # Docs: https://requests.readthedocs.io/en/latest/user/quickstart/#make-a-request
 API_URL = "https://restful-booker.herokuapp.com"
@@ -7,32 +8,38 @@ API_URL = "https://restful-booker.herokuapp.com"
 
 # API endpoints
 def health_check() -> Response:
-	url = API_URL + ""
+	url = API_URL + "/ping"
 	response = request(url=url, method="GET")
 	return response
 
 
+# def health_checker() -> Response:
+# 	response2 = requests.get(API_URL + "/ping")
+# 	return response2
+
+
 def create_booking(request_payload: Dict[str, Any]) -> Response:
-	url = ""
-	response = request(url=url, method="", data=request_payload)
+	url = API_URL + "/booking"
+	headers = {"Content-Type": "application/json"}
+	response = request(url=url, method="POST", data=request_payload, headers=headers)
 	return response
 
 
 def delete_booking(booking_id: str, headers: Dict[str, str]) -> Response:
-	url = ""
-	response = request(url=url, method="", headers=headers)
+	url = API_URL + "/booking/1"
+	response = request(url=url, method="DELETE", headers=headers)
 	return response
 
 
-def get_booking_by_id():
-	pass
+# def get_booking_by_id():
+# 	pass
 
 
-def get_list_of_bookings():
-	pass
+# def get_list_of_bookings():
+# 	pass
 
 
 # You can have two separate functions to call different update-endpoints or one to handle both based on a condition
-def update_booking():
-	pass
+# def update_booking():
+# 	pass
 
--- requests-practice/tests.py
+++ requests-practice/tests.py
@@ -1,13 +1,34 @@
+import json
+
 from restful_booker_API import health_check, create_booking
+
+X = {
+        "firstname": "Jim",
+        "lastname": "Brown",
+        "totalprice": 111,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+    }
 
 
 # API tests
 def test_health_check():
-	response = health_check()
-	assert response.status_code == 201
+    response = health_check()
+    assert response.status_code == 201
+    assert response.url == "https://restful-booker.herokuapp.com/ping"
+
+# assert response. == "GET"
 
 
 def test_create_booking():
-	payload = {}
-	json_response = create_booking(request_payload=payload).json()
-	assert json_response
+    payload = X
+    response = create_booking(request_payload=payload)
+    assert response.url == "https://restful-booker.herokuapp.com/booking"
+    # json_response = response.json()
+    # assert "bookingid" in json_response
+    # assert "application/json" in response.headers["Content-Type"]
+    # assert response.status_code == 200
